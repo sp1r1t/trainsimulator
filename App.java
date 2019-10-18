01ad80487c8754d6a6369c63219b669cb18b62f0
Julian Konrath <offic@jinn.at>
2019-10-18 17:47:39 +0200

    Train Class added

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
 public class App
 {
- 
-       public static void main (String[] args)
-       {
-             // Ausgabe Hello World!
-             System.out.println("Hello World!");
-       }
+      public static void main (String[] args)
+      {
+            // Ausgabe Hello World!
+            System.out.println("Hello World!");
+            Train train = new Train();
+      }
 }
